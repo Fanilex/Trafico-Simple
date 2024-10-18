b1fa366090d2776202b7424304bf9e2c16e8cb69
A01736897 <a01736897@tec.mx>
2024-10-17 22:55:06 -0600

    Pregunta 3: Slider

--- webapi.jl
+++ webapi.jl
@@ -1,53 +1,55 @@
-
 include("simple.jl")
 using Genie, Genie.Renderer.Json, Genie.Requests, HTTP
 using UUIDs
 
 
-# Guarda las instancias
+# Store the instances
 const instances = Dict{String, TrafficModel}()
 
 
 route("/simulations", method = POST) do
-model = initialize_model()
-id = string(uuid1())
-instances[id] = model
-cars = []
-for car in model.agents
-push!(cars, Dict("id" => car.id, "pos" => car.pos))
-end
-json(Dict("Location" => "/simulations/$id", "cars" => cars))
+  payload = jsonpayload()
+  speed = parse(Float64, get(payload, "speed", "1.0"))  # Parse the speed from the payload
+  model = initialize_model(speed)
+  id = string(uuid1())
+  instances[id] = model
+  cars = []
+  for car in model.agents
+      push!(cars, Dict("id" => car.id, "pos" => car.pos))
+  end
+  json(Dict("Location" => "/simulations/$id", "cars" => cars))
 end
 
 
 route("/simulations/:id", method = GET) do
-id = params(:id)
-if haskey(instances, id)
-model = instances[id]
-# actualiza los agentes
-for car in model.agents
-agent_step!(car, model)
-end
-# semaforos
-light_horizontal, light_vertical = model.traffic_lights
-cycle_light!(light_horizontal)
-cycle_light!(light_vertical)
-# posiciones de los coches
-cars = []
-for car in model.agents
-push!(cars, Dict("id" => car.id, "pos" => car.pos))
-end
-json(Dict(
-"cars" => cars,
-"traffic_lights" => Dict(
-"horizontal" => light_horizontal.state,
-"vertical" => light_vertical.state
-)
-))
-else
-HTTP.status(404)
-json(Dict("error" => "Simulation with ID $id not found"))
-end
+  id = params(:id)
+  if haskey(instances, id)
+      speed = parse(Float64, get(params(), "speed", "1.0"))  # Ensure speed is parsed correctly
+      model = instances[id]
+      # Update agents with the given speed
+      for car in model.agents
+          agent_step!(car, model, speed)
+      end
+      # Update traffic lights
+      light_horizontal, light_vertical = model.traffic_lights
+      cycle_light!(light_horizontal)
+      cycle_light!(light_vertical)
+      # Car positions
+      cars = []
+      for car in model.agents
+          push!(cars, Dict("id" => car.id, "pos" => car.pos))
+      end
+      json(Dict(
+          "cars" => cars,
+          "traffic_lights" => Dict(
+              "horizontal" => light_horizontal.state,
+              "vertical" => light_vertical.state
+          )
+      ))
+  else
+      HTTP.status(404)
+      json(Dict("error" => "Simulation with ID $id not found"))
+  end
 end
 
 
@@ -59,3 +61,6 @@ Genie.config.cors_allowed_origins = ["*"]
 
 
 up()
+
+
+
